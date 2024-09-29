2b01178136ba1185bb8eee47dc1ef1fb6a08c19a
Nikita Kharkovsky <superman_7@inbox.ru>
2024-09-29 14:26:30 +0300

    add tests

---
+++ test_add.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include "my_add.hpp"
+
+TEST(Add, Simple) {
+    EXPECT_EQ(my_add(3,3), 6);
+    EXPECT_EQ(my_add(4, 8), my_add(8, 4));
+    EXPECT_EQ(my_add(-45, 9), -36);
+}
