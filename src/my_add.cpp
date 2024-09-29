c3b6507dc78bf691a2748e396d724273d5b97800
Nikita Kharkovsky <superman_7@inbox.ru>
2024-09-29 17:46:44 +0300

    add auto documentation

--- src/my_add.cpp
+++ src/my_add.cpp
@@ -1,3 +1,11 @@
+/**
+ * @brief Calculates sum of two integer values a and b.
+ * 
+ * @param[in] a a
+ * @param[in] b b
+ * @return Sum of a and b
+ */
+
 int my_add(int a, int b) {
     return a + b;
 }
